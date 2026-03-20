3144212ac23af2522e1d6c3e079ed38f9e54977d
Reyesjessica <22620158@tlaxiaco.tecnm.mx>
2026-03-19 21:04:36 -0600

    Validadacion de campos

--- Usuario/src/main/java/com/jessica/usuario/model/Usuario.java
+++ Usuario/src/main/java/com/jessica/usuario/model/Usuario.java
@@ -1,9 +1,9 @@
 package com.jessica.usuario.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,10 +17,23 @@ import lombok.Setter;
 public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private Long id;
+
+    @NotBlank(message = "El nombre es obligatorio")
+    private String nombre;
+
+    @NotBlank(message = "Es obligatorio este apartado")
     private String apellidos;
+
+    @Email(message = "El correo debe ser valido")
+    @NotBlank(message = "El correo es obligatorio")
+    @Column(unique = true)
     private String correo;
+
+
+    @Pattern(regexp = "^[0-9]{10}$", message = "El telefono debe ser valido")
     private String telefono;
-    private String direccion;
+
+
+    private  String direccion;
 }
