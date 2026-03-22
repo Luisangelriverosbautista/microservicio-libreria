c5f698439f6daa2571268b8f83a9b3d321a9118e
Reyesjessica <22620158@tlaxiaco.tecnm.mx>
2026-03-22 15:15:01 -0600

    Definir el controlador de libro

---
+++ Libro/src/main/java/com/jessica/libro/controller/LibroController.java
@@ -0,0 +1,50 @@
+package com.jessica.libro.controller;
+
+import com.jessica.libro.model.Libro;
+import com.jessica.libro.services.LibroServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/libros")
+@CrossOrigin(origins = "*")
+public class LibroController {
+    @Autowired
+    private LibroServices libroServices;
+
+    @GetMapping("/traer-libros")
+    public List<Libro> traerLibros() {
+        return libroServices.Obtnertodos();
+    }
+
+    @GetMapping("/traer-libro/{id}")
+    public ResponseEntity<Libro> traerUnLibro(@PathVariable Long id) {
+        Optional<Libro> libro = libroServices.obtenerPorId(id);
+        return libro.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+
+    @PostMapping("/insertar-libro")
+    public Libro insertarLibro(@RequestBody Libro libro) {
+        return libroServices.crearLibro(libro);
+    }
+
+
+    @PutMapping("/editar-libro/{id}")
+    public ResponseEntity<Libro> actualizarlibro(@PathVariable Long id, @RequestBody Libro libro) {
+        Optional<Libro> actualizado = libroServices.editaLibro(id, libro);
+        return actualizado.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+
+    @DeleteMapping("/eliminar-libro/{id}")
+    public ResponseEntity<Void> eliminarlibro(@PathVariable Long id) {
+        libroServices.eliminarLibro(id);
+        return ResponseEntity.ok().build();
+    }
+}
