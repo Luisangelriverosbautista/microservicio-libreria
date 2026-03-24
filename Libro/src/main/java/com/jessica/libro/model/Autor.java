54f655a9f86546fc7f0e97b56622cfc3601af24e
Aylin67 <158499297+Aylin67@users.noreply.github.com>
2026-03-23 22:07:27 -0600

    Agregar relaciones Autor y categoria

--- Libro/src/main/java/com/jessica/libro/model/Autor.java
+++ Libro/src/main/java/com/jessica/libro/model/Autor.java
@@ -1,12 +1,10 @@
 package com.jessica.libro.model;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import java.util.List;
 
 @Entity
 @Getter
@@ -18,4 +16,6 @@ public class Autor {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idAutor;
     private String nombre;
+    @ManyToMany(mappedBy = "autores")
+    private List<Libro> libros;
 }
--- Libro/src/main/java/com/jessica/libro/model/Categoria.java
+++ Libro/src/main/java/com/jessica/libro/model/Categoria.java
@@ -1,13 +1,10 @@
 package com.jessica.libro.model;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
+import java.util.List;
 @Entity
 @Getter
 @Setter
@@ -18,4 +15,6 @@ public class Categoria {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombre;
+    @ManyToMany(mappedBy = "autores")
+    private List<Libro> libros;
 }
